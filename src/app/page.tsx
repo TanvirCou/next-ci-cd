1a58ec0ed0287a274de57114f1cf0d7731b0cb09
Tanvir Ahmed <ahmed.tnvr999@gmail.com>
2024-11-19 02:16:12 +0600

    I am trying to add ci-cd to same file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <p>Yeeeeee,Successfully learn ci-cd pipeline with github actions</p>
+      <p>
+        Yeeeeee,Successfully added ci-cd pipeline in same file with github
+        actions
+      </p>
     </div>
   );
 }
